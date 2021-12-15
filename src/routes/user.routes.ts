bd162122c9fb158b5127257faabc2261dc3eca37
Bruno <mguimaraesbruno@gmail.com>
2021-12-15 16:26:06 -0300

    Code Review: Brunão

--- src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import UserController from '../controllers/UserController';
-
-const userRouter = Router();
-
-userRouter.post('/', UserController.create);
-
-export default userRouter;
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import './setup';
-
-app.listen(process.env.PORT, () => {
-  console.clear();
-  console.log(`Env: ${process.env.NODE_ENV}`);
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
